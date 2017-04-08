8644986247ec5a97029f986b325890cc0272bd46
Vladislav <kouklevv@gmail.com>
2017-04-08 15:42:08 +0300

    Improved create user method and better example

--- api.py
+++ api.py
@@ -17,12 +17,21 @@ class TelegraphAPIClient(BaseClient):
         self.params = None
         self.data = None
 
-    async def create_account(self):
+    async def create_account(self, short_name=None, author_name=None, author_url=None):
         self.url = self.BASE_URL.format('createAccount')
+        if not short_name:
+            short_name = config.SHORT_NAME
+
+        if not author_name:
+            author_name = config.AUTHOR_NAME
+
+        if not author_url:
+            author_url = config.AUTHOR_URL
+
         self.params = {
-            'short_name': config.SHORT_NAME,
-            'author_name': config.AUTHOR_NAME,
-            'author_url': config.AUTHOR_URL,
+            'short_name': short_name,
+            'author_name': author_name,
+            'author_url': author_url,
         }
         result = await self.get_request()
         return result
--- examples.py
+++ examples.py
@@ -2,6 +2,18 @@ import asyncio
 
 from api import TelegraphAPIClient
 
+async def register_user():
+    short_name = input('Enter your short name: ')
+    author_name = input('Enter author name: ')
+    author_url = input('Enter your author url: ')
+    response = await tg.create_account(short_name, author_name, author_url)
+    if response['ok']:
+        result = response['result']
+        return result
+    else:
+        return False
+
+
 async def get_posts_count():
     response = await tg.get_page_list()
     if response['ok']:
@@ -13,8 +25,18 @@ async def get_posts_count():
 
 
 async def main(loop):
-    posts = await get_posts_count()
-    print(posts)
+    user = await register_user()
+    if user:
+        token = user['access_token']
+        auth_url = user['auth_url']
+        tg.ACCESS_TOKEN = token
+        count = await get_posts_count()
+        print('Your access token: {}'.format(token))
+        print('You can authentificate in browser by this url: {}'.format(auth_url))
+        print('Your posts count is {}'.format(count))
+    else:
+        print("Something gone wrong")
+
 
 loop = asyncio.get_event_loop()
 tg = TelegraphAPIClient()
